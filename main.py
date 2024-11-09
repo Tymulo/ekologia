613b6ebc1777320fa67e22b88e39518f34a8c2bc
Tymulo <tymkozlow@gmail.com>
2024-11-09 19:02:55 +0100

    pygame

--- main.py
+++ main.py
@@ -24,9 +24,33 @@ def gtime(time_speed, pause):
         cr_time.normalizetime()
         cr_time.print_time()
 
+
 if __name__ == "__main__":
     time_speed = 1
     pause = False
-    gtime(time_speed, pause)
+    c
 
+
+    # import pygame
+    # pygame.init()
+    # X = 500
+    # Y = 500
+    # white = (255, 255, 255)
+    # green = (0, 255, 0)
+    # blue = (0, 0, 128)
+    
+    # display_surface = pygame.display.set_mode((X, Y))
+    # pygame.display.set_caption("simulation")
+    # font = pygame.font.Font('freesansbold.ttf', 32)
+    # text = font.render(gtime(time_speed, pause), True, green, blue)
+    # textRect = text.get_rect()
+    # textRect.center = (X // 2, Y // 2)
+    # exit = False
+    # while not exit: 
+    #     display_surface.fill(white)
+    #     display_surface.blit(text, textRect)
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT: 
+    #             exit = True
+    # pygame.display.update() 
 
