23dc69b0e117a8a762922d439f0461655414169f
Tymulo <tymkozlow@gmail.com>
2024-11-13 17:27:54 +0100

    introduced time in py game

--- main.py
+++ main.py
@@ -13,42 +13,48 @@ class Time():
             self.hour = 0
 
     def print_time(self):
-        print(f"Day: {self.day} {self.hour:02}:{self.minute:02}")
-
-def gtime(time_speed, pause):
-    import time
-    cr_time = Time(00,00,00)
-    while pause != True:
-        time.sleep(1 // time_speed)
-        cr_time.minute += 1
-        cr_time.normalizetime()
-        cr_time.print_time()
-
+        return(f"Day: {self.day} {self.hour:02}:{self.minute:02}")
 
 if __name__ == "__main__":
-    time_speed = 1
+    import time
+    import ctypes
+    import pygame
+
+    cr_time = Time(0, 0, 0)
+    time_speed = 2
     pause = False
 
-    # import pygame
-    # pygame.init()
-    # X = 500
-    # Y = 500
-    # white = (255, 255, 255)
-    # green = (0, 255, 0)
-    # blue = (0, 0, 128)
-    
-    # display_surface = pygame.display.set_mode((X, Y))
-    # pygame.display.set_caption("simulation")
-    # font = pygame.font.Font('freesansbold.ttf', 32)
-    # text = font.render(gtime(time_speed, pause), True, green, blue)
-    # textRect = text.get_rect()
-    # textRect.center = (X // 2, Y // 2)
-    # exit = False
-    # while not exit: 
-    #     display_surface.fill(white)
-    #     display_surface.blit(text, textRect)
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT: 
-    #             exit = True
-    # pygame.display.update() 
+    pygame.init()
+    X, Y = 500, 500
+    white = (255, 255, 255)
+    green = (0, 255, 0)
+    blue = (0, 0, 128)
+    pygame.display.set_caption("Simulation")
+    display_surface = pygame.display.set_mode((X, Y))
+    font = pygame.font.Font('freesansbold.ttf', 18)
+
+    text_x, text_y = 250, 250
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pause = not pause
+        if not pause:
+            time.sleep(1 / abs(time_speed))
+            cr_time.minute += 1
+            cr_time.normalizetime()
+        # else:
+        #     pygame.draw.rect()
+        time_text = font.render(cr_time.print_time(), True, white, blue)
+        textRect = time_text.get_rect(center = (text_x, text_y))
+
+        display_surface.fill(blue)
+        display_surface.blit(time_text, textRect)
+        pygame.display.flip()
+
+    pygame.quit()
 
